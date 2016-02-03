334ee3c62f83397f0de1f3e8a7032235f0ff8cf8
Nguyễn Quang Hòa <hoanqhd@gmail.com>
2016-02-03 15:01:50 +0700

    fix custom title bar page

--- custom-func.php
+++ custom-func.php
@@ -10,9 +10,12 @@
  */
 if( ! function_exists( 'rt_replace_color_title_in_archive' ) ) {
 	function rt_replace_color_title_in_archive( $str = '' ) {
-		$str2 = ( explode( " ", $str ) );
-		$str3 = array_shift( $str2 );
-		return '<span class="first-character">'. $str3 .'</span> ' . implode( " ", $str2 );
-		
+		$array = ( explode( " ", $str ) );
+		$separate_arr = ceil( count( $array )/2 );
+		$array_child_1 = array_slice( $array, 0, $separate_arr );
+		$array_child_2 = array_slice( $array, $separate_arr );
+		$string_child_1 = implode( $array_child_1,' ');
+		$string_child_2 = implode( $array_child_2,' ');
+		return '<span class="first-character">'. $string_child_1 .'</span> ' . $string_child_2;
 	}
 }
