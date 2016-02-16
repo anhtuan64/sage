3fe764298921ec14fe73f78bdb581bcc40543834
anhtuan64 <anhtuan180808@gmail.com>
2016-02-16 10:05:26 +0700

    Fix bug on single teacher page

--- single-post-k-teacher.php
+++ single-post-k-teacher.php
@@ -19,39 +19,40 @@
 					<?php the_content(); ?>
 				</div>
 				<div class="realate-post"> 
-					<?php 
-						$relate = get_field('courses'); 
-						$name = "KHÓA HỌC ĐANG GIẢNG DẠY";
-					?>
-					<p class="entry-title"> <?php echo rt_replace_color_title_in_archive($name); ?> </p>
-					<?php 
+				<?php 
+					$relate = get_field('courses'); 
+					$name = "KHÓA HỌC ĐANG GIẢNG DẠY";
+				 
+					if( is_array( $relate ) && count( $relate ) > 0 ) {
+						echo '<p class="entry-title">'. rt_replace_color_title_in_archive($name) .'</p>';
 						for ( $i = 0; $i < count( $relate ); $i++ ) { 
-						$url = wp_get_attachment_image_src( get_post_thumbnail_id($relate[$i]['course_of_teacher']->ID,'medium') );
-						$link = $relate[$i]['course_of_teacher']->guid;
-						$title = $relate[$i]['course_of_teacher']->post_title;
-						$excerpt = $relate[$i]['course_of_teacher']->post_excerpt;
-						if ( ( $i % 2 == 0 ) ) {
-							echo "<div class='box-relate'>";
-						}
-					?>
-						<div class="list-relate">
-							<a class="img" href="<?php echo $link; ?>">
-								<img src="<?php echo $url[0]; ?>" alt="" />
-							</a>
-							<a class="title-relate" href="<?php echo $link; ?>" title="<?php echo $title;?>">
-								<?php echo $title;?> 
-							</a>
-							<div class="excerpt"> 
-								<?php echo substr($excerpt, 0,80); ?>
-								<a class="read-more" href="<?php the_permalink();?>" title="<?php the_title();?>"><?php echo __( '... Readmore ', RT_LANGUAGE ) ?></a>
-							</div>
+							$url 		= wp_get_attachment_image_src( get_post_thumbnail_id($relate[$i]['course_of_teacher']->ID,'medium') );
+							$link 		= get_post_permalink( $relate[$i]['course_of_teacher']->ID );
+							$title 		= $relate[$i]['course_of_teacher']->post_title;
+							$excerpt 	= $relate[$i]['course_of_teacher']->post_excerpt;
+							if ( ( $i % 2 == 0 ) ) {
+								echo "<div class='box-relate'>";
+							}
+				?>
+					<div class="list-relate">
+						<a class="img" href="<?php echo $link; ?>">
+							<img src="<?php echo $url[0]; ?>" alt="" />
+						</a>
+						<a class="title-relate" href="<?php echo $link; ?>" title="<?php echo $title;?>">
+							<?php echo $title;?> 
+						</a>
+						<div class="excerpt"> 
+							<?php echo substr($excerpt, 0,80); ?>
+							<a class="read-more" href="<?php echo $link; ?>" title="<?php echo $title;?>"><?php echo __( '... Readmore ', RT_LANGUAGE ) ?></a>
 						</div>
-					<?php
-						if ( ( $i % 2 != 0 ) || ( $i == count( $relate ) - 1 ) ) {
-							echo "</div>";
-						}
+					</div>
+				<?php
+							if ( ( $i % 2 != 0 ) || ( $i == count( $relate ) - 1 ) ) {
+								echo "</div>";
+							}
 						} // end for
-					?>
+					} // end if
+				?>
 				</div> <!-- end post relate post -->
 			</div> <!-- end post inner -->
 		</div> <!-- end post id -->
